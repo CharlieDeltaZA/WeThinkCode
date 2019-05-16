75ef3e62a00c6b36e7ca7fcb8d909d8b8c82e48a
Calvin Diogo <calvin@diogo.co.za>
2019-05-16 16:09:43 +0200

    Update ft_strstr.c

--- Year1/S1/libft/ft_strstr.c
+++ Year1/S1/libft/ft_strstr.c
@@ -2,5 +2,20 @@
 
 char	*ft_strstr(const char *haystack, const char *needle)
 {
-	//TODO
+	int i;
+	int j;
+
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] == haystack[i + j])
+		{
+			if (needle[j + 1] == '\0')
+				return (haystack + i);
+			j++;
+		}
+
+		i++;
+	}
 }

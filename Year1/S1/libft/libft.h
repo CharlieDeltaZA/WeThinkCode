e469ba738badf5b258a646d866ad22d9f1cdd698
Calvin Diogo <calvin@diogo.co.za>
2019-05-16 16:28:26 +0200

    Added strcmp, strncmp

--- Year1/S1/libft/libft.h
+++ Year1/S1/libft/libft.h
@@ -11,5 +11,7 @@ void	ft_putstr_fd(char const *s, int fd);
 char    *ft_strcpy(char *dst, const char *src);
 char    *ft_strncpy(char *dst, const char *src, size_t len);
 char    *ft_strstr(const char *haystack, const char *needle);
+int     ft_strcmp(const char *s1, const char *s2)
+int     ft_strncmp(const char *s1, const char *s2, size_t n)
 
 #endif

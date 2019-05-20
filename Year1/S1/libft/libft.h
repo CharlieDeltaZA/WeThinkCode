d123608218b6dde5ea9a65f796f8912275f7a1c7
Event WeThinkCode <event@c3r3s3.wethinkcode.co.za>
2019-05-20 09:34:40 +0200

    Added skeletons str{nstr, cat, ncat, lcat, chr, rchr}

--- Year1/S1/libft/libft.h
+++ Year1/S1/libft/libft.h
@@ -30,5 +30,11 @@ char    *ft_strdup(const char *s1);
 int     ft_atoi(const char *str);
 void    ft_putendl(char const *s);
 void    ft_putendl_fd(char const *s, int fd);
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
+char    *ft_strchr(const char *s, int c);
+char    *ft_strrchr(const char *s, int c);
+char	*ft_strcat(char *s1, const char *s2);
+char	*ft_strncat(char *s1, const char *s2, size_t n);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
 #endif

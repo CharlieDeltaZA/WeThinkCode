faf895b9d8f060df4dd8f055151698dad7eafe29
CharlieDeltaZA <cdiogo@student.wethinkcode.co.za>
2019-05-20 12:37:45 +0200

    Added ft_strdel

--- Year1/S1/libft/libft.h
+++ Year1/S1/libft/libft.h
@@ -46,5 +46,6 @@ int		memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
 char	*ft_strnew(size_t size);
+void	ft_strdel(char **as);
 
 #endif

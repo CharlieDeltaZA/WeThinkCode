41d30da03400c9840409d4b2aedfebe58b876661
Calvin Diogo <calvin@diogo.co.za>
2019-05-21 11:24:11 +0200

    Fixed norm error: Tabs not spaces

--- Year1/S1/libft/libft.h
+++ Year1/S1/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:24:59 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/21 09:25:09 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/21 11:23:51 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,29 +22,29 @@ void	ft_putchar(char c);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr(char const *s);
 void	ft_putstr_fd(char const *s, int fd);
-char    *ft_strcpy(char *dst, const char *src);
-char    *ft_strncpy(char *dst, const char *src, size_t len);
-char    *ft_strstr(const char *haystack, const char *needle);
-int     ft_strcmp(const char *s1, const char *s2);
-int     ft_strncmp(const char *s1, const char *s2, size_t n);
-int     ft_toupper(int c);
-int     ft_tolower(int c);
-int     ft_islower(int c);
-int     ft_isupper(int c);
-int     ft_isalpha(int c);
-int     ft_isdigit(int c);
-int     ft_isalnum(int c);
-int     ft_isascii(int c);
-int     ft_isprint(int c);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
+char	*ft_strstr(const char *haystack, const char *needle);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_islower(int c);
+int		ft_isupper(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 void	ft_putnbr(int n);
 void	ft_putnbr_fd(int n, int fd);
-char    *ft_strdup(const char *s1);
-int     ft_atoi(const char *str);
-void    ft_putendl(char const *s);
-void    ft_putendl_fd(char const *s, int fd);
-char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
-char    *ft_strchr(const char *s, int c);
-char    *ft_strrchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+int		ft_atoi(const char *str);
+void	ft_putendl(char const *s);
+void	ft_putendl_fd(char const *s, int fd);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 char	*ft_strcat(char *s1, const char *s2);
 char	*ft_strncat(char *s1, const char *s2, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);

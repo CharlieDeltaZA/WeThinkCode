a75580c09df3f1e9c4c1568db279ade455b4a48c
Calvin Diogo <calvin@diogo.co.za>
2019-05-21 14:35:16 +0200

    Updated strchr, strrchr - need work

--- Year1/S1/libft/ft_strchr.c
+++ Year1/S1/libft/ft_strchr.c
@@ -6,22 +6,23 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:39:13 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/21 13:54:15 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/21 14:29:34 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// this works but doesn't return null when the char doesn't exist
+
 char	*ft_strchr(const char *s, int c)
 {
-	char	*ret;
 	int		i;
 
 	i = 0;
-	ret = NULL;
 	while (s[i] != c)
 		i++;
 	if (s[i] == c)
-		ret = &s[i];
-	return ((char*)ret);
+		return ((char*)&s[i]);
+	else
+		return (NULL);
 }
--- Year1/S1/libft/ft_strrchr.c
+++ Year1/S1/libft/ft_strrchr.c
@@ -6,13 +6,25 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:45:41 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/21 09:45:43 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/21 14:31:35 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// this works but doesn't return null when the char doesn't exist
+
 char	*ft_strrchr(const char *s, int c)
 {
-	//TODO
+	size_t		i;
+
+	i = ft_strlen(s);
+	i += 1;
+
+	while (s[i] != c)
+		i--;
+	if (s[i] == c)
+		return ((char*)&s[i]);
+	else
+		return (NULL);
 }

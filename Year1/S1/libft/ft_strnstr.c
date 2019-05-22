6274edd3b67f436da100a4ca7079042cf0b0afba
Calvin Diogo <calvin@diogo.co.za>
2019-05-22 15:49:41 +0200

    Update strnstr, needs work

--- Year1/S1/libft/ft_strnstr.c
+++ Year1/S1/libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:45:31 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/21 09:45:32 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/22 14:35:29 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	//TODO
+	int i;
+	int j;
+
+	i = 0;
+	if (ft_strlen(needle) == 0)
+		return (haystack);
+	while (haystack[i] != '\0' && i <= len)
+	{
+		j = 0;
+		while (needle[j] == haystack[i + j])
+		{
+			if (needle[j + 1] == '\0')
+				return ((char*)&haystack[i]);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
 }

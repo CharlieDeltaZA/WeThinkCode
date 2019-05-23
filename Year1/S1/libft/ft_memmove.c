4abf41a7995a11dc72d0e459fc17c27ae4fcf493
Calvin Diogo <calvin@diogo.co.za>
2019-05-23 16:04:44 +0200

    update memmove, still needs to support overlap

--- Year1/S1/libft/ft_memmove.c
+++ Year1/S1/libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:32:29 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/23 15:35:34 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/23 15:56:35 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,19 @@
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	//TODO
+	size_t		i;
+	char		*dest;
+	const char	*source;
+
+	dest = dst;
+	source = src;
+	i = 0;
+	if (dest == NULL && source == NULL)
+		return (dest);
+	while (i < len)
+	{
+		dest[i] = source[i];
+		i++;
+	}
 	return (dst);
 }

59c2790689492b6502009a3b46cdee9e2c76a6bf
Calvin Diogo <calvin@diogo.co.za>
2019-05-23 12:26:16 +0200

    Fixed putnbr_fd : Check for INT_MIN

--- Year1/S1/libft/ft_putnbr_fd.c
+++ Year1/S1/libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:36:17 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/21 11:17:20 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/23 12:24:47 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n < 0)
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else
 	{
-		ft_putchar_fd('-', fd);
-		n *= -1;
+		if (n < 0)
+		{
+			ft_putchar_fd('-', fd);
+			n *= -1;
+		}
+		if (n >= 10)
+			ft_putnbr_fd((n / 10), fd);
+		ft_putchar_fd(((n % 10) + '0'), fd);
 	}
-	if (n >= 10)
-		ft_putnbr_fd(n / 10, fd);
-	ft_putchar_fd((n % 10) + '0', fd);
 }

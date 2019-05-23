46765925c917ea4ce3ee445811295d001e5e466c
Calvin Diogo <calvin@diogo.co.za>
2019-05-23 20:46:37 +0200

    Update memchr - working

--- Year1/S1/libft/ft_memchr.c
+++ Year1/S1/libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:31:58 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/21 09:31:59 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/23 20:44:12 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	//TODO
+	size_t		i;
+	unsigned char	*str;
+
+	i = 0;
+	str = ((unsigned char*)s);
+	while (i < n)
+	{
+		if (str[i] == (unsigned char)c)
+			return (str + i);
+		i++;
+	}
+	return (NULL);
+
 }

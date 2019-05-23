c618f30647839861456c2e2d6b7dd7c03cff8407
Calvin Diogo <calvin@diogo.co.za>
2019-05-23 14:10:35 +0200

    update memcpy - working

--- Year1/S1/libft/ft_memcpy.c
+++ Year1/S1/libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:31:50 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/21 09:31:51 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/23 14:08:18 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	//TODO
+	char		*dest;
+	const char	*source;
+	size_t		i;
+
+	dest = dst;
+	source = src;
+	i = 0;
+	if (dest == NULL && source == NULL)
+		return (dest);
+	while (i < n)
+	{
+		dest[i] = source[i];
+		i++;
+	}
+	return (dest);
 }

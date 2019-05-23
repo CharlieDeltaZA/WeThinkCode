2bdff291dcc4ed84b9913cba7dd3791eef7dfa08
Calvin Diogo <calvin@diogo.co.za>
2019-05-23 21:13:09 +0200

    Update ft_memcmp.c

--- Year1/S1/libft/ft_memcmp.c
+++ Year1/S1/libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:32:05 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/23 15:34:45 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/23 21:08:52 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,20 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	//TODO
-	
-	if (ft_strlen(s1) == 0 && ft_strlen(s2) == 0)
-		return (0);
+	size_t		i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	i = 0;
+	str1 = (unsigned char*)s1;
+	str2 = (unsigned char*)s2;
+	while (i < n)
+	{
+		if (str1[i] > str2[i])
+			return (str1[i] - str2[i]);
+		if (str1[i] < str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
 }

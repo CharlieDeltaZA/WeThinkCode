0f70cd50e4453e233920802c65f33fe951de5e3f
Calvin Diogo <calvin@diogo.co.za>
2019-05-23 12:06:32 +0200

    Fixed strrchr

--- Year1/S1/libft/ft_strrchr.c
+++ Year1/S1/libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:45:41 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/23 11:09:45 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/23 11:57:51 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strrchr(const char *s, int c)
 	//size_t	i;
 	int		i;
 
-	i = (ft_strlen(s) + 1);
+	i = (ft_strlen(s));
 	while (i >= 0 && s[i] != c)
 		i--;
 	if (!(s[i] == c))

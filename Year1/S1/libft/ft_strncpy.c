d63e6737d4640e9471fc3db7d1de8613e9a3593c
Calvin Diogo <calvin@diogo.co.za>
2019-05-24 12:58:42 +0200

    update strncpy - working

--- Year1/S1/libft/ft_strncpy.c
+++ Year1/S1/libft/ft_strncpy.c
@@ -6,42 +6,26 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:53:03 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/23 21:42:03 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/24 12:58:18 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// I don't think this handles src being < len char long
-// Also needs to handle not terminating the dst if no space in *dst
-
 char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
 	size_t i;
-	size_t srclen;
-	size_t dstlen;
 
 	i = 0;
-	srclen = ft_strlen(src);
-	dstlen = ft_strlen(dst);
-//	while (src[i] != '\0' && (i < len))
-//	{
-//		dst[i] = src[i];
-//		i++;
-//	}
-//	dst[i] = '\0';
+	while (i < len && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
 	while (i < len)
-		{
-			dst[i] = src[i];
-			i++;
-		}
-	if (srclen < len)
-		{
-			while (dstlen >= len)
-			{
-				dst[dstlen] = '\0';
-				dstlen--;
-			}
-		}
+	{
+		dst[i] = '\0';
+		i++;
+	}
 	return (dst);
 }

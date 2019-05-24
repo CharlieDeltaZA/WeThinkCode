a84f161878c6c6998c3cb27dd0c7b7616c7431c7
Calvin Diogo <calvin@diogo.co.za>
2019-05-24 08:42:48 +0200

    Normed atoi - 29 lines > 25

--- Year1/S1/libft/ft_atoi.c
+++ Year1/S1/libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:25:18 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/24 08:38:10 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/24 08:42:11 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,21 @@ int	ft_atoi(const char *str)
 	neg = 1;
 	res = 0;
 	while (str[i] != '\0' && (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
-		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r'))
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r' || str[i] == '+'))
 		i++;
 	if (str[i] == '-')
 	{
 		neg = -1;
 		i++;
 	}
-	if (str[i] == '+')
-		i++;
 	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
 	{
 		if (res > 469762049 && neg == 1)
-            return (-1);
-        else if (res > 469762049 && neg == -1)
-            return (0);
-		res *= 10;
-		res = res + str[i] - '0';
+			return (-1);
+		else if (res > 469762049 && neg == -1)
+			return (0);
+		res = (res * 10) + (str[i] - '0');
 		i++;
 	}
-	res = res * neg;
-	return (res);
+	return (res * neg);
 }

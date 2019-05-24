96d28d26f36d1b40198bd6e315db6fbd0b85ad00
Calvin Diogo <calvin@diogo.co.za>
2019-05-24 15:42:48 +0200

    Update strlcat - working

--- Year1/S1/libft/ft_strlcat.c
+++ Year1/S1/libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:44:13 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/21 09:44:14 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/24 15:42:25 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	//TODO
+	size_t	dstlen;
+	size_t	i;
+
+	dstlen = 0;
+	while ((dst[dstlen] != '\0') && dstlen < dstsize)
+		dstlen++;
+	i = dstlen;
+	while ((src[dstlen - i] != '\0') && (dstlen + 1 < dstsize))
+	{
+		dst[dstlen] = src[dstlen - i];
+		dstlen++;
+	}
+	if (i < dstsize)
+		dst[dstlen] = '\0';
+	return (i + ft_strlen(src));
 }

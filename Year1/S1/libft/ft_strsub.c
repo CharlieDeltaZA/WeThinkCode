5101a738a05f424a333d3b66c01f08dd73a88cf8
Calvin Diogo <calvin@diogo.co.za>
2019-05-27 09:27:14 +0200

    updated strsub - working

--- Year1/S1/libft/ft_strsub.c
+++ Year1/S1/libft/ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:45:50 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/21 09:45:51 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/27 09:25:43 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,21 @@
 
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
-	//TODO
-	return (NULL);
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	if (s == NULL)
+		return (NULL);
+	str = (char*)malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	while (i < len && s[start] != '\0')
+	{
+		str[i] = s[start];
+		start++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }

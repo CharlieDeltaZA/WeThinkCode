8d8f9b72c5f7306f95f62e7c1c5de22fa574af88
Calvin Diogo <calvin@diogo.co.za>
2019-05-27 10:14:43 +0200

    Updated strjoin - working

--- Year1/S1/libft/ft_strjoin.c
+++ Year1/S1/libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:44:02 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/21 09:44:03 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/27 10:14:12 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,26 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	//TODO
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	str = (char*)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[j++] = s1[i++];
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		str[j++] = s2[i++];
+	}
+	str[j] = '\0';
+	return (str);
 }

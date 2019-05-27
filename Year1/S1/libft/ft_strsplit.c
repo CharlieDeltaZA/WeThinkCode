f565c3776e3c1435891d20a5ce627d36cae03a4a
Calvin Diogo <calvin@diogo.co.za>
2019-05-27 14:43:58 +0200

    updates to strtrim, strsplit

--- Year1/S1/libft/ft_strsplit.c
+++ Year1/S1/libft/ft_strsplit.c
@@ -6,11 +6,15 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 10:19:37 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/27 10:20:04 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/27 12:59:51 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	**ft_strsplit(char const *s, char c)
 {
 	//TODO
+	int		i;
+	int		count;
+
+	
 }
--- Year1/S1/libft/ft_strtrim.c
+++ Year1/S1/libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 10:17:27 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/27 12:10:53 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/27 14:20:47 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,14 @@ char	*ft_strtrim(char const *s)
 
     if (s == NULL)
 		return (NULL);
+	if (ft_strlen(s) == 0)
+		return ((char*)s);
 	i = 0;
     x = 0;
     l = ft_strlen(s) - 1;
-    while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+    while ((s[i] == ' ' || s[i] == '\t' || s[i] == '\n') && s[i] != '\0')
         i++;
-    while (s[l] == '\t' || s[l] == '\n' || s[l] == ' ')
+    while ((s[l] == '\t' || s[l] == '\n' || s[l] == ' ') && l >= 0)
         l--;
     nstr = (char *)malloc(sizeof(char) * (l - i) + 2);
 //	nstr = ft_strnew((l - i) + 1);

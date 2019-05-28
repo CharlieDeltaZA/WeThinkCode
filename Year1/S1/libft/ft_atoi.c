054ea0eb09bf03521b0cef2c3198aa6211cdb18d
Calvin Diogo <calvin@diogo.co.za>
2019-05-28 13:16:13 +0200

    updates to strncmp, atoi

--- Year1/S1/libft/ft_atoi.c
+++ Year1/S1/libft/ft_atoi.c
@@ -6,11 +6,18 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:25:18 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/24 08:42:11 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/28 13:09:43 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+**		if (res > 469762049 && neg == 1)
+**			return (-1);
+**		else if (res > 469762049 && neg == -1)
+**			return (0);
+*/
 
 int	ft_atoi(const char *str)
 {
@@ -22,19 +29,16 @@ int	ft_atoi(const char *str)
 	neg = 1;
 	res = 0;
 	while (str[i] != '\0' && (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
-		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r' || str[i] == '+'))
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r'))
 		i++;
-	if (str[i] == '-')
+	if (str[i] == '-' || str[i] == '+')
 	{
-		neg = -1;
+		if (str[i] == '-')
+			neg = -1;
 		i++;
 	}
 	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
 	{
-		if (res > 469762049 && neg == 1)
-			return (-1);
-		else if (res > 469762049 && neg == -1)
-			return (0);
 		res = (res * 10) + (str[i] - '0');
 		i++;
 	}

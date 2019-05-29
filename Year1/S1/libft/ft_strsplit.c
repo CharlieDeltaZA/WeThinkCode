bf680d1b104f383beb265d6250d9e9e195e6659e
Calvin Diogo <calvin@diogo.co.za>
2019-05-29 16:17:30 +0200

    Small update and comments

--- Year1/S1/libft/ft_strsplit.c
+++ Year1/S1/libft/ft_strsplit.c
@@ -6,9 +6,15 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 10:19:37 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/28 10:37:25 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/29 16:17:08 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+ * Count doesn't work if string doesn't start with a delim
+ * Need to code a sub-string length func
+ * Need to code the core of the main func
+*/ 
 
 static int	count_words(char const *s, char d)
 {
@@ -17,7 +23,7 @@ static int	count_words(char const *s, char d)
 
 	i = 0;
 	count = 0;
-	while (s[i] != '\0')
+	while (s[i + 1] != '\0')
 	{
 		if (s[i] == d && s[i + 1] != d && s[i] != '\0')
 		{

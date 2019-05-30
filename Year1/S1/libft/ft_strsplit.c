91990ef39183b9d3bcadc34eeae7c0f2ac9b20cc
Calvin Diogo <calvin@diogo.co.za>
2019-05-30 15:19:58 +0200

    I tried things. Send help

--- Year1/S1/libft/ft_strsplit.c
+++ Year1/S1/libft/ft_strsplit.c
@@ -6,9 +6,11 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 10:19:37 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/30 12:17:36 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/30 15:15:20 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 /*
  * Count doesn't work if string doesn't start with a delim > Recursion, Thanks Cameron
@@ -32,13 +34,16 @@ static int	count_words(char const *str, char delim, int index)
 	}
 	return (count + count_words(str, delim, index));
 }
+
 // this needs an index starting at the first char that isn't a delim
-static int	str_len(char *str, char delim, int index)
+static int	str_len(char const *str, char delim, int index)
 {
 	int		len;
 
 	len = 0;
-	while (str[index] != delim)
+	while (str[index] == delim)
+		index++;
+	while (str[index] != delim && str[index] != '\0')
 	{
 		index++;
 		len++;
@@ -50,7 +55,29 @@ char		**ft_strsplit(char const *s, char c)
 {
 	//TODO
 	int		i;
-	int		count;
+	int		j;
+	char	**array;
+	int		wcount;
+	int		k;
 
-	
+	if (!s || !c) //can I do that?!?!?!
+		return (NULL);
+	wcount = count_words(s, c, 0);
+	if (!(array = (char**)malloc(sizeof(*array) * wcount + 1)))
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (i++ < wcount)
+	{
+		k = 0;
+		if (!(array[i] = ft_strnew(str_len(s, c, j))))
+			return (NULL);
+		while (s[j] == c)
+			j++;
+		while (s[j] != c && s[j])
+			array[i][k++] = s[j++];
+		array[i][k] = '\0';
+	}	
+	array[i] = 0;
+	return (array);
 }

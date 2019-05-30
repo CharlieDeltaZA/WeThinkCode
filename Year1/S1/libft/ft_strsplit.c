29175ba126189a116a0f0202c8f0730aeaa54b32
Calvin Diogo <calvin@diogo.co.za>
2019-05-30 11:14:55 +0200

    add substring length func

--- Year1/S1/libft/ft_strsplit.c
+++ Year1/S1/libft/ft_strsplit.c
@@ -6,13 +6,13 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 10:19:37 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/29 16:17:08 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/30 11:14:38 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
  * Count doesn't work if string doesn't start with a delim
- * Need to code a sub-string length func
+ * Need to code a sub-string length func > DONE
  * Need to code the core of the main func
 */ 
 
@@ -36,6 +36,19 @@ static int	count_words(char const *s, char d)
 	if (s[i] == '\0')
 		count = 1;
 	return (count);
+}
+// this needs an index starting at the first char that isn't a delim
+static int	str_len(char *str, char d, int index)
+{
+	int		len;
+
+	len = 0;
+	while (str[index] != d)
+	{
+		index++;
+		len++;
+	}
+	return (len);
 }
 
 char		**ft_strsplit(char const *s, char c)

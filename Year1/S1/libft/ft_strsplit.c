bbac81fdf62044fd2b1d5abe456dbffa8a96acef
Calvin Diogo <calvin@diogo.co.za>
2019-05-30 12:25:52 +0200

    Count_words works

--- Year1/S1/libft/ft_strsplit.c
+++ Year1/S1/libft/ft_strsplit.c
@@ -6,43 +6,39 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 10:19:37 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/30 11:59:09 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/30 12:17:36 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
- * Count doesn't work if string doesn't start with a delim
+ * Count doesn't work if string doesn't start with a delim > Recursion, Thanks Cameron
  * Need to code a sub-string length func > DONE
  * Need to code the core of the main func
 */ 
 
-static int	count_words(char const *s, char d)
+static int	count_words(char const *str, char delim, int index)
 {
-	int		i;
 	int		count;
 
-	i = 0;
+	if (!(str[index]))
+		return (0);
 	count = 0;
-	while (s[i] == d)
-		i++;
-	else
+	while (str[index] == delim)
+		index++;
+	while (str[index] != '\0' && str[index] != delim)
 	{
-		count++;
-		while (!(ft_iswhitespace(s[i]) && s[i] && s[i] != d))
-			i++;
+		index++;
+		count = 1;
 	}
-
-//	if (s[i] == '\0')
-//		count = 1;
-	return (count);
+	return (count + count_words(str, delim, index));
 }
 // this needs an index starting at the first char that isn't a delim
-static int	str_len(char *str, char d, int index)
+static int	str_len(char *str, char delim, int index)
 {
 	int		len;
 
 	len = 0;
-	while (str[index] != d)
+	while (str[index] != delim)
 	{
 		index++;
 		len++;

96b2917755105b0d099bf3370a3c26eaba0dec74
Calvin Diogo <calvin@diogo.co.za>
2019-05-31 08:35:32 +0200

    Fixed alignment as per campus macs

--- Year1/S1/libft/libft.h
+++ Year1/S1/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:24:59 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/30 20:35:23 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/05/31 08:32:17 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void				*ft_memcpy(void *dst, const void *src, size_t n);
 void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void				*ft_memmove(void *dst, const void *src, size_t len);
 void				*ft_memchr(const void *s, int c, size_t n);
-int				ft_memcmp(const void *s1, const void *s2, size_t n);
+int					ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t				ft_strlen(const char *str);
 char				*ft_strdup(const char *s1);
 char				*ft_strcpy(char *dst, const char *src);
@@ -44,16 +44,16 @@ char				*ft_strrchr(const char *s, int c);
 char				*ft_strstr(const char *haystack, const char *needle);
 char				*ft_strnstr(const char *haystack, const char *needle, \
 									size_t len);
-int				ft_strcmp(const char *s1, const char *s2);
-int				ft_strncmp(const char *s1, const char *s2, size_t n);
-int				ft_atoi(const char *str);
-int				ft_isalpha(int c);
-int				ft_isdigit(int c);
-int				ft_isalnum(int c);
-int				ft_isascii(int c);
-int				ft_isprint(int c);
-int				ft_toupper(int c);
-int				ft_tolower(int c);
+int					ft_strcmp(const char *s1, const char *s2);
+int					ft_strncmp(const char *s1, const char *s2, size_t n);
+int					ft_atoi(const char *str);
+int					ft_isalpha(int c);
+int					ft_isdigit(int c);
+int					ft_isalnum(int c);
+int					ft_isascii(int c);
+int					ft_isprint(int c);
+int					ft_toupper(int c);
+int					ft_tolower(int c);
 
 /*
 ** Part 2
@@ -68,8 +68,8 @@ void				ft_striter(char *s, void (*f)(char *));
 void				ft_striteri(char *s, void (*f)(unsigned int, char *));
 char				*ft_strmap(char const *s, char (*f)(char));
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-int				ft_strequ(char const *s1, char const *s2);
-int				ft_strnequ(char const *s1, char const *s2, size_t n);
+int					ft_strequ(char const *s1, char const *s2);
+int					ft_strnequ(char const *s1, char const *s2, size_t n);
 char				*ft_strsub(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s);
@@ -88,21 +88,21 @@ void				ft_putnbr_fd(int n, int fd);
 ** Bonus
 */
 
-typedef struct			s_list
+typedef struct		s_list
 {
 	void			*content;
 	size_t			content_size;
-	struct s_list		*next;
-}				t_list;
+	struct s_list	*next;
+}					t_list;
 
 /*
 ** Extra
 */
 
-int				ft_islower(int c);
-int				ft_isupper(int c);
-int				ft_iswhitespace(char c);
-int				ft_intlen(int n);
+int					ft_islower(int c);
+int					ft_isupper(int c);
+int					ft_iswhitespace(char c);
+int					ft_intlen(int n);
 char				*ft_strrev(char *str);
 
 

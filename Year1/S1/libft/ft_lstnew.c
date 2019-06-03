8bd203bc5fb655580b0d4f7fd722539a140862e1
Calvin Diogo <calvin@diogo.co.za>
2019-06-03 15:56:51 +0200

    Updated lstnew - working

--- Year1/S1/libft/ft_lstnew.c
+++ Year1/S1/libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 09:56:24 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/06/03 09:58:24 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/06/03 15:56:36 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
-	//TODO
+	t_list	*list;
+
+	if (!(list = (t_list*)malloc(sizeof(*list))))
+		return (NULL);
+	if (content == NULL)
+	{
+		list->content = NULL;
+		list->content_size = 0;
+	}
+	else
+	{
+		if (!(list->content = malloc(content_size)))
+			return (NULL);
+		//list->content = (void*)content;
+		ft_memcpy(list->content, content, content_size);
+		list->content_size = content_size;
+	}
+	list->next = NULL;
+	return (list);
 }

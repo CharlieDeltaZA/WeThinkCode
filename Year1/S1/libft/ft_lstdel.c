6aa79b36d39194fcebbdcfa600c3fc771e0363a2
Calvin Diogo <calvin@diogo.co.za>
2019-06-03 16:53:29 +0200

    Added lstdel

--- Year1/S1/libft/ft_lstdel.c
+++ Year1/S1/libft/ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 10:00:42 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/06/03 10:02:00 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/06/03 16:53:19 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
-	//TODO
+	t_list	*current;
+	t_list	*next;
+
+	current = *alst;
+	while (current)
+	{
+		next = current->next;
+		del(current->content, current->content_size);
+		free(current);
+		current = next;
+	}
+	*alst = NULL;
 }

88e4a0f52400d7133349fd3d91e5439f98f106d3
Calvin Diogo <calvin@diogo.co.za>
2019-06-03 10:06:59 +0200

    Added lst funcs, need work

--- Year1/S1/libft/libft.h
+++ Year1/S1/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:24:59 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/31 11:38:46 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/06/03 09:55:35 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,6 +94,12 @@ typedef struct		s_list
 	size_t			content_size;
 	struct s_list	*next;
 }					t_list;
+t_list				*ft_lstnew(void const *content, size_t content_size);
+void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstadd(t_list **alst, t_list *new);
+void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 
 /*
 ** Extra
@@ -106,6 +112,5 @@ int					ft_isspace(int c);
 int					ft_intlen(int n);
 char				*ft_strrev(char *str);
 char				*ft_strndup(const char *s1, size_t n);
-
 
 #endif

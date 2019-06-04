417395a5594b231e8137dfdeb11eba6f3e5a437a
Calvin Diogo <calvin@diogo.co.za>
2019-06-04 13:29:04 +0200

    update lstiter - fixed logic -- working

--- Year1/S1/libft/ft_lstiter.c
+++ Year1/S1/libft/ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 10:03:15 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/06/04 13:04:02 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/06/04 13:28:29 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,11 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
-	//TODO
 	if (lst != NULL && f != NULL)
 	{
-		lst = lst->next;
 		while (lst != NULL)
 		{
-			//lst = lst->next;
-			f(lst->content);
+			f(lst);
 			lst = lst->next;
 		}
 	}

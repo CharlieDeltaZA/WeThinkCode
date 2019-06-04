d1fd9a096a94b57201b216baff0a058f773fc656
Calvin Diogo <calvin@diogo.co.za>
2019-06-04 14:43:55 +0200

    Update strcmp, strncmp - changed return values

--- Year1/S1/libft/ft_strcmp.c
+++ Year1/S1/libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:39:30 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/24 16:22:30 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/06/04 14:37:33 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ int	ft_strcmp(const char *s1, const char *s2)
 	i = 0;
 	while ((s1[i] != '\0') || (s2[i] != '\0'))
 	{
-		if (s1[i] > s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		if (s1[i] < s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		if ((unsigned char)s1[i] > (unsigned char)s2[i])
+			return (TRUE);
+		if ((unsigned char)s1[i] < (unsigned char)s2[i])
+			return (-1);
 		i++;
 	}
-	return (0);
+	return (FALSE);
 }

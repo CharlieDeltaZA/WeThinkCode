900aa77f19c029f086fe2acbe17274730acebd39
Calvin Diogo <calvin@diogo.co.za>
2019-06-04 09:52:31 +0200

    Added lstadd - working, but fails forbidden func check

--- Year1/S1/libft/ft_lstadd.c
+++ Year1/S1/libft/ft_lstadd.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 10:02:12 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/06/03 10:05:06 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/06/04 09:49:41 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,10 @@
 void	ft_lstadd(t_list **alst, t_list *new)
 {
 	//TODO
+	t_list	*new_node;
+
+	new_node = ft_lstnew(new->content, new->content_size);
+	new_node->next = *alst;
+	*alst = new_node;
+
 }

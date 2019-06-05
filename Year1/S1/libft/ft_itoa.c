db0471714f510d1a32df092a14de3a89e463fc28
Calvin Diogo <calvin@diogo.co.za>
2019-06-05 11:01:30 +0200

    Added comments, renamed 1 static func

--- Year1/S1/libft/ft_itoa.c
+++ Year1/S1/libft/ft_itoa.c
@@ -6,11 +6,16 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:31:17 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/05/29 14:02:19 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/06/05 11:00:31 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Helper function for ft_itoa.
+** Checks whether the int passed is negative or not.
+*/
 
 static int	is_neg(int n)
 {
@@ -22,7 +27,12 @@ static int	is_neg(int n)
 	return (i);
 }
 
-static char	*check_min(char *minint)
+/*
+** Helper function for ft_itoa.
+** If `n` is INT_MIN, code runs to malloc a string of the value for INT_MIN.
+*/
+
+static char	*check_min_int(char *minint)
 {
 	int		i;
 	char	*min;
@@ -37,6 +47,12 @@ static char	*check_min(char *minint)
 	return (min);
 }
 
+/*
+** Allocates and returns a fresh null terminated string, representing the int
+** `n` passed as argument. Negative numbers are supported. NULL returned on
+** allocation failure.
+*/
+
 char		*ft_itoa(int n)
 {
 	int		len;
@@ -45,7 +61,7 @@ char		*ft_itoa(int n)
 	char	*num;
 
 	if (n == -2147483648)
-		return (check_min(MININT));
+		return (check_min_int(MININT));
 	len = ft_intlen(n);
 	neg = is_neg(n);
 	i = len;

8bb72a57d12be93378f5912be4f313c8e9cdefc3
Calvin Diogo <calvin@diogo.co.za>
2019-06-06 15:44:07 +0200

    Defined BUFF_SIZE

--- Year1/S1/get_next_line/get_next_line.h
+++ Year1/S1/get_next_line/get_next_line.h
@@ -6,12 +6,14 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 15:19:45 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/06/06 15:21:04 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/06/06 15:43:55 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 42
 
 int		get_next_line(const int fd, char **line);
 

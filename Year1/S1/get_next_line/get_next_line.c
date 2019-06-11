88819fff8f45d30993b0731c44044997c9b14632
Calvin Diogo <calvin@diogo.co.za>
2019-06-11 12:48:40 +0200

    Re-add Makefile, update gnl.c

--- Year1/S1/get_next_line/get_next_line.c
+++ Year1/S1/get_next_line/get_next_line.c
@@ -6,13 +6,48 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 15:18:49 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/06/10 09:59:50 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/06/11 12:48:05 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		get_next_line(const int fd, char **line)
+/*
+** Comment Here
+*/
+
+static int	new_line(char **str,char **line, const int fd, int ret)
+{
+	char	*temp;
+	int		len;
+
+	len = 0;
+	while (str[fd][len] != '\n'  && str[fd][len] != '\0')
+		len++;
+	if (str[fd][len] == '\n')
+	{
+		*line = ft_strsub(str[fd], 0, len);
+		temp = ft_strdup(str[fd] + len + 1);
+		free(str[fd]);
+		str[fd] = temp;
+		if (str[fd][0] == '\0')
+			ft_strdel(&str[fd]);
+	}
+	else if (str[fd][len] == '\0')
+	{
+		if (ret == BUFF_SIZE)
+			return (get_next_line(fd, line));
+		*line = ft_strdup(str[fd]);
+		ft_strdel(&str[fd]);
+	}
+	return (TRUE);
+}
+
+/*
+** Comment Here
+*/
+
+int			get_next_line(const int fd, char **line)
 {
 	//TODO
 }

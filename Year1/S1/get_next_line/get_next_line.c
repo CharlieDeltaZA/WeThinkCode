bdb0afbdcd5e9b8808a90e9a349af28f6acf971a
Calvin Diogo <calvin@diogo.co.za>
2019-06-13 10:30:30 +0200

    removed commented main

--- Year1/S1/get_next_line/get_next_line.c
+++ Year1/S1/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 15:18:49 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/06/12 14:59:27 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/06/13 10:30:21 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,34 +73,3 @@ int			get_next_line(const int fd, char **line)
 		return (FALSE);
 	return (new_line(str, line, fd, ret));
 }
-
-/*
-**int	main(int argc, char **argv)
-**{
-**	int		fd;
-**	char	**line;
-**	int		gnlret;
-**	int		linecount;
-**
-**	linecount = 0;
-**	gnlret = 1;
-**	fd = open(argv[1], O_RDONLY);
-**	printf("Argc: %d\n", argc);
-**	printf("File to open: %s\n", argv[1]);
-**	line = ft_memalloc(sizeof(char **));
-**	printf("BUFF_SIZE: %d\n\n\n", BUFF_SIZE);
-**	while (gnlret > 0)
-**	{
-**		gnlret = get_next_line(fd, line);
-**		printf("%s\n", *line);
-**		linecount++;
-**	//	ft_putnbr(linecount);
-**	//	ft_putendl("");
-**	//	printf("gnlret = |%d|\n", gnlret);
-**	}
-**	printf("Line count: %d\n", linecount);
-**	printf("Finished\n\n");
-**	close(fd);
-**	return (0);
-**}
-*/

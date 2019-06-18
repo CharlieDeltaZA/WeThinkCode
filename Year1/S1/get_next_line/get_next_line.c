218680e280171d6066a5f95773846560a17a1caa
Calvin Diogo <calvin@diogo.co.za>
2019-06-18 13:01:43 +0200

    Code, but hep

--- Year1/S1/get_next_line/get_next_line.c
+++ Year1/S1/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 08:27:38 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/06/18 11:00:23 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/06/18 12:57:39 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,67 @@
 // Read the fd with read()
 //
 
-int			get_next_line(const int fd. char **line)
+static int	find_n(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\n' && str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int			get_next_line(const int fd, char **line)
 {
 	//TODO
-	int		ret;
-	char	buffer[BUFF_SIZE + 1];
+	int			ret;
+	char		buffer[BUFF_SIZE + 1];
+	static char *str;
+	char		*tmp;
+	char		*extra;
+	size_t		i;
 
+	if (line == NULL)
+		return (-1);
+
+	str = ft_strdup("");
+	while ((ret = read(fd, buffer, BUFF_SIZE)))
+	{
+		buffer[ret] = '\0';
+		tmp = ft_strjoin(str, buffer);
+		free(str);
+		str = tmp;
+		//strchr
+		if (ft_strchr(buffer, '\n'))
+		{
+			i = find_n(str);
+			if (str[i] == '\n')
+			{
+				*line = ft_strsub(str, 0, i);
+				extra = ft_strdup(str + 1 + i);
+				free(str);
+				str = extra;
+			}
+			else if (str[i] == '\0')
+			{
+				if (ret == BUFF_SIZE)
+					return (get_next_line(fd, line)); //?
+				*line = ft_strdup(str);
+			}
+		}
+	}
+	if (ret < 0)
+		return (-1);
+	else if (ret == 0)
+		return (0);
+	else
+		return (1);
+	//*line = copyuntil \n character.
+	//tmp = str;
+	//str = (strdup) str + \n location + 1
+	//free(tmp);
+	//printf("%s\n", str);
+	return (0);
 }
 
 // Declaring, allocating & passing an empty var **line to store lines found by gnl

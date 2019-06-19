6586808fb8a13d0f6b842f9f6ee0117b0675572d
Calvin Diogo <calvin@diogo.co.za>
2019-06-19 13:35:51 +0200

    Changes

--- Year1/S1/get_next_line/get_next_line.c
+++ Year1/S1/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 08:27:38 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/06/19 11:51:56 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/06/19 13:28:03 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ static size_t	find_n(char *str)
 		i++;
 	return (i);
 }
-/*
+
 static int		newline(const int fd, char **line, int ret, char *str, int i)
 {
 	char	*extra;
@@ -37,19 +37,21 @@ static int		newline(const int fd, char **line, int ret, char *str, int i)
 		extra = ft_strdup(str + 1 + i);
 		free(str);
 		str = extra;
-		//tmp = extra;
-		return (1);
+		if (str[0] == '\0')
+			ft_strdel(&str);
+		//return (1);
 	}
 	else if (str[i] == '\0')
 	{
 		if (ret == BUFF_SIZE)
 			return (get_next_line(fd, line)); //?
 		*line = ft_strdup(str);
-		return (1);
+		ft_strdel(&str);
+		//return (1);
 	}
-	return (0);
+	return (1);
 }
-*/
+
 int				get_next_line(const int fd, char **line)
 {
 	//TODO
@@ -71,29 +73,34 @@ int				get_next_line(const int fd, char **line)
 		tmp = ft_strjoin(str, buffer);
 		free(str);
 		str = tmp;
-		if (ft_strchr(str, '\n'))
+		if (ft_strchr(buffer, '\n'))
 		{
 			i = find_n(str);
+			break ;
 			//return(newline(fd, line, ret, str, i));
-			if (str[i] == '\n')
+/*			if (str[i] == '\n')
 			{
 				*line = ft_strsub(str, 0, i);
 				extra = ft_strdup(str + 1 + i);
 				free(str);
 				str = extra;
-				return (1);
+	//			return (1);
 			}
 			else if (str[i] == '\0')
 			{
 				if (ret == BUFF_SIZE)
 					return (get_next_line(fd, line)); //?
 				*line = ft_strdup(str);
-				return (1);
-			}
-	//		return (1);
-		}
+	//			return (1);
+			} 
+			return (1);
+	*/	} 
 	}
+	if (ret < 0)
+		return (-1);
+	else if (ret == 0)
 		return (0);
+	return (newline(fd, line, ret, str, i));
 	// > *line = copyuntil \n character.
 	// > tmp = str;
 	// > str = (strdup) str + \n location + 1
--- Year1/S1/get_next_line/main.c
+++ Year1/S1/get_next_line/main.c
@@ -2,13 +2,15 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-int		main(int ac, char **av)
+//int		main(int ac, char **av)
+int		main(void)
 {
 	char	**line;
 	int		gnl_ret = 1;
 	int		fd;
 
-	fd = open(av[1], O_RDONLY);
+	//fd = open(av[1], O_RDONLY);
+	fd = open("short_test.txt", O_RDONLY);
 	line = malloc(sizeof(char**));
 	while ((gnl_ret = get_next_line(fd, line)) > 0)
 	{

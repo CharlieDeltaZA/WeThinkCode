c5cc43651163374c20151ac8519c7e3059519747
Calvin Diogo <calvin@diogo.co.za>
2019-06-20 11:25:01 +0200

    removed unneccessary comments and code

--- Year1/S1/get_next_line/get_next_line.c
+++ Year1/S1/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 08:27:38 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/06/20 11:21:30 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/06/20 11:24:32 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,6 @@ static int		newline(const int fd, char **line, int ret, char **str)
 	char	*extra;
 	size_t	i;
 
-//	i = 0;
-//	while (str[fd][i] != '\n' && str[fd][i] != '\0')
-//		i++;
 	i = find_n(str, fd);
 	if (str[fd][i] == '\n')
 	{
@@ -57,13 +54,10 @@ static int		newline(const int fd, char **line, int ret, char **str)
 
 int				get_next_line(const int fd, char **line)
 {
-	//TODO
 	int			ret;
 	char		buffer[BUFF_SIZE + 1];
 	static char *str[64];
 	char		*tmp;
-//	char		*extra;
-//	size_t		i;
 
 	if (fd < 0 || line == NULL || read(fd, buffer, 0))
 		return (-1);
@@ -72,39 +66,16 @@ int				get_next_line(const int fd, char **line)
 	{
 		if (str[fd] == NULL)
 			str[fd] = ft_strdup("");
-//			str = ft_strnew(1);
 		buffer[ret] = '\0';
 		tmp = ft_strjoin(str[fd], buffer);
 		free(str[fd]);
 		str[fd] = tmp;
 		if (ft_strchr(buffer, '\n'))
-		{
-//			i = find_n(str, fd);
 			break ;
-/*			if (str[fd][i] == '\n')
-			{
-				*line = ft_strsub(str[fd], 0, i);
-				extra = ft_strdup(str[fd] + 1 + i);
-				free(str[fd]);
-				str[fd] = extra;
-			}
-			else if (str[fd][i] == '\0')
-			{
-				if (ret == BUFF_SIZE)
-					return (get_next_line(fd, line)); //?
-				*line = ft_strdup(str[fd]);
-			} 
-			return (1); */
-		} 
 	}
 	if (ret < 0)
 		return (-1);
 	else if (ret == 0 && (str[fd] == NULL || str[fd] == '\0'))
 		return (0);
 	return (newline(fd, line, ret, str));
-//	return (1);
-													// > *line = copyuntil \n character.
-													// > tmp = str;
-													// > str = (strdup) str + \n location + 1
-													// > free(tmp);
 }

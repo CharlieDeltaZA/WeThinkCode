5a7690064017ebbbce65ad00688a63766c12dd87
Calvin Diogo <calvin@diogo.co.za>
2019-06-20 11:49:21 +0200

    Prettied up the main file a bit

--- Year1/S1/get_next_line/main.c
+++ Year1/S1/get_next_line/main.c
@@ -2,22 +2,27 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-//int		main(int ac, char **av)
-int		main(void)
+int		main(int ac, char **av)
+//int		main(void)
 {
 	char	**line;
 	int		gnl_ret = 1;
 	int		fd;
+	size_t	count = 0;
 
-	//fd = open(av[1], O_RDONLY);
-	fd = open("short_test.txt", O_RDONLY);
+	fd = open(av[1], O_RDONLY);
+//	fd = open("short_test.txt", O_RDONLY);
 	line = malloc(sizeof(char**));
+	printf("-------Start of File-------\n");
 	while ((gnl_ret = get_next_line(fd, line)) > 0)
 	{
 		printf("%s\n", *line);
 		free(*line);
+		count++;
 	}
+	printf("-----------EOF-------------\n");
 	close(fd);
-	printf("\nFIN :)\n");
+	printf("%zu lines printed\n", count);
+	printf("FIN :D\n");
 	return (0);
 }
